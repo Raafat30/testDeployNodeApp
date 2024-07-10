fa0e08cffbbda28b20c6a645241dbd67bc364535
Raafat30 <raafatzidi39@gmail.com>
2024-07-10 18:17:15 +0100

    files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const http = require('http');
+const server = http.createServer((req,res)=>{
+
+  res.writeHead(200,{"Content-type":"text/plain"})
+  res.end("Hello World");
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT,()=> console.log("server is runing in port 3000"))
